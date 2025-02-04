6da97eaf16db46028bc7eb4a34991de8ba6cc026
AdityaPatel149 <55062974+adityapatel149@users.noreply.github.com>
2025-02-03 22:56:12 -0800

    Update generate_mesh_from_pcd.py

--- generate_mesh_from_pcd.py
+++ generate_mesh_from_pcd.py
@@ -1,8 +1,8 @@
 import open3d
 import numpy as np
 
-pcd = open3d.io.read_point_cloud(".\meshes\test (15).jpg.ply")
-
+# pcd = open3d.io.read_point_cloud(".\meshes\test (15).jpg.ply")
+pcd = open3d.io.read_point_cloud(".\pointclouds\merged.ply")
 mesh, densities = open3d.geometry.TriangleMesh.create_from_point_cloud_poisson(pcd, depth=12, n_threads=4)
 rotation = mesh.get_rotation_matrix_from_xyz((np.pi,0,0))
 mesh.rotate(rotation, center=(0,0,0))
